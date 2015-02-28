a4c61303a3ccdf732231127049f16b93f318a771
andrew castro <listerine1989@gmail.com>
2015-02-28 17:01:51 -0500

    soporte para local storage del apikey
    base init

--- config/config.js
+++ config/config.js
@@ -1,20 +1,39 @@
 'use strict'
 
 var config = (function(suite){
-	/* todo el manejo del api key aqui */
+	var _prefix = 'suite_';
 	var _apiKey;
-	
+	var _store = this.window.localStorage;
+
 	var _key = function(key){
 		_apiKey = key;
-		/* guardar apikey en localstorage */
+		_store.setItem( _prefix + 'apikey', key);
 	}
 
 	var key = function(key){
 		_key(key);
 	}
 
+	var _clear = function(){
+		_store.removeItem(_prefix + 'apikey');
+	}
+
+	var clear = function(){
+		_clear();
+	}
+
+	var _getKey = function(){
+		return _store.getItem(_prefix + 'apikey');
+	}
+
+	var getKey = function(){
+		return _getKey();
+	} 
+
 	suite.config = {
-		key : key
+		key   : key,
+		clear : clear,
+		getKey :getKey
 	}
 
 	return suite;
